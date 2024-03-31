7a3311438a933e404596dff513584e952f341f68
palak-khan <palak.khan.2633@gmail.com>
2024-03-31 15:10:39 +0530

    completed

--- script.js
+++ script.js
@@ -48,3 +48,77 @@ let swiperPopular = new Swiper(".popular--container", {
     },
   },
 });
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section");
+
+function scrollActive() {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      current.classList.add("active-link");
+    } else {
+      current.classList.remove("active-link");
+    }
+  });
+}
+const sr = ScrollReveal({
+  origin:'top',
+  distance:'60px',
+  duration:2500,
+  delay:400,
+  // reset:true,
+})
+sr.reveal(`.home--title ,.popular--container ,.features--img ,.featured--filters`)
+sr.reveal(`.home--subtitle`,{
+  delay:500,
+})
+sr.reveal(`.home--elec`,{
+  delay:600,
+})
+sr.reveal(`.home--img`,{
+  delay:800,
+})
+sr.reveal(`.home--car-data`,{
+  delay:900,
+  interval:100,
+  origin:'bottom'
+})
+sr.reveal(`.home--button`,{
+  delay:1000,
+  origin:'bottom'
+})
+sr.reveal(`.about--group ,.offer--data`,{
+  origin:'left'
+})
+sr.reveal(`.about--data ,.offer--img  `,{
+  origin:'right'
+})
+sr.reveal(`.about--data`,{
+  origin:'right'
+})
+sr.reveal(`.features--map`,{
+  delay:600,
+  origin:'bottom'
+})
+sr.reveal(`.features--card`,{
+ interval:300
+})
+sr.reveal(`.featured--card`,{
+ interval:100
+})
+sr.reveal(`.featured--card ,.logos--content ,.footer--content`,{
+ interval:100
+})
